d955f01132f9c33984b8eba566f87315b4ec41b2
Evaristo-Paulo <ivarilson909@gmail.com>
2021-07-17 17:44:15 +0100

    updating project

--- config/database.php
+++ config/database.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://ljzygiruinhiyn:025939befa736883f094b92c5ad056aa57e1cf24e8fe4a76df56b40ecdd9dfd0@ec2-52-45-183-77.compute-1.amazonaws.com:5432/d6ikfimlq96ori');
+
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], '/'),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
